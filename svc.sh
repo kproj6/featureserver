9b6343274c67af6c6d92fa1ee9b3fd3ba77ca6d4
Arve Nygård <arve.nygard@comoyo.com>
2014-10-23 15:04:47 +0200

    Add deploy script.
    You can deploy the server by running
    `./deploy.sh` .
    This requires that you have added your public key on the server.

---
+++ svc.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# Runs the java program as a background service.
+SERVICE_NAME=featureserver
+PATH_TO_JAR=/home/featureserver/featureserver/target/featureserver-0.1-SNAPSHOT-jar-with-dependencies.jar
+ARGS="--netcdf-file=/home/featureserver/files/samples_2013.08.05_large.nc"
+PID_PATH_NAME=/tmp/featureserver.pid
+case $1 in
+    start)
+        echo "Starting $SERVICE_NAME ..."
+        if [ ! -f $PID_PATH_NAME ]; then
+            nohup java -jar $PATH_TO_JAR $ARGS 2>> /dev/null >> /dev/null &
+                        echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is already running ..."
+        fi
+    ;;
+    stop)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stoping ..."
+            kill $PID;
+            echo "$SERVICE_NAME stopped ..."
+            rm $PID_PATH_NAME
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+    restart)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stopping ...";
+            kill $PID;
+            echo "$SERVICE_NAME stopped ...";
+            rm $PID_PATH_NAME
+            echo "$SERVICE_NAME starting ..."
+            nohup java -jar $PATH_TO_JAR $ARGS 2>> /dev/null >> /dev/null &
+                        echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+esac
