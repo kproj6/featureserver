c25274d3ab42eec0f7d3c4dc6d79a8eba80f7992
Ondrej Hujnak <ondrejh@stud.ntnu.no>
2014-10-25 14:59:43 +0200

    Refactor of current magnitude. And moved from horrible NetCdfManager.Variable to Feature enum ;)

--- src/main/java/com/sintef/featureserver/netcdf/Feature.java
+++ src/main/java/com/sintef/featureserver/netcdf/Feature.java
@@ -4,9 +4,12 @@ package com.sintef.featureserver.netcdf;
  * Created by Emil on 18.10.2014.
  */
 public enum Feature {
-    SALINITY,
-    TEMPERATURE,
-    CURRENT_MAGNITUDE,
-    CURRENT_DIRECTION,
-    DEPTH
+	DEPTH,
+	SALINITY,
+	TEMPERATURE,
+	WATER_VELOCITY,
+	WIND_VELOCITY,
+	WTF_VELOCITY, // @TODO Change to whatever w_velocity in Sintefs' NetCDFs mean
+	CURRENT_MAGNITUDE,
+    CURRENT_DIRECTION
 }
